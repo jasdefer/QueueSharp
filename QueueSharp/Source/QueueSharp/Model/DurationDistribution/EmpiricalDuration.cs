e380b8a760fb3a2ad7c3a0ca359f4dc3e9d22b71
Justus Bonz <justus.bonz@gmail.com>
2025-03-17 13:54:53 +0100

    Add normal distributed duration

--- QueueSharp/Source/QueueSharp/Model/DurationDistribution/EmpiricalDuration.cs
+++ QueueSharp/Source/QueueSharp/Model/DurationDistribution/EmpiricalDuration.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections.Immutable;
 
 namespace QueueSharp.Model.DurationDistribution;
-internal record EmpiricalDuration : IDurationDistribution
+
+/// <summary>
+/// Generate a duration based on empirical data.
+/// </summary>
+public record EmpiricalDuration : IDurationDistribution
 {
     private readonly ImmutableArray<int> _empiricalDurations;
     private readonly bool _randomizeOrder;
     private int _index = 0;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="empiricalDurations">The list of duration, from which random values are selected.</param>
+    /// <param name="randomizeOrder">True, if the order of values is random. False, if the values are selected based on their order.</param>
     public EmpiricalDuration(IEnumerable<int> empiricalDurations, bool randomizeOrder = true)
     {
         _empiricalDurations = empiricalDurations.ToImmutableArray();
         _randomizeOrder = randomizeOrder;
         _index = _empiricalDurations.Length;
     }
+
+    /// <summary>
+    /// Get the next duration value.
+    /// </summary>
     public int GetDuration(Random? random = null)
     {
         random ??= new Random();
---
+++ QueueSharp/Source/QueueSharp/Model/DurationDistribution/NormalDistributedDuration.cs
@@ -0,0 +1,59 @@
+﻿using MathNet.Numerics.Distributions;
+using System.Collections.ObjectModel;
+
+namespace QueueSharp.Model.DurationDistribution;
+
+/// <summary>
+/// Generate a normal distributed duration.
+/// </summary>
+public class NormalDistributedDuration : IDurationDistribution
+{
+    /// <summary>
+    /// The mean parameter of the normal distribution
+    /// </summary>
+    private readonly double _mean;
+
+    /// <summary>
+    /// The variance parameter of the normal distribution
+    /// </summary>
+    private readonly double _variance;
+
+    /// <summary>
+    /// Values are capped at this minimum value. If null, no minimum is enforced.
+    /// </summary>
+    private readonly int? _min;
+
+    /// <summary>
+    /// Values are capped at this maximum value. If null, no maximum is enforced.
+    /// </summary>
+    private readonly int? _max;
+
+    /// <summary>
+    /// Constructor to initialize the normal distributed duration parameters.
+    /// </summary>
+    public NormalDistributedDuration(double mean, double variance, int? min = null, int? max = null)
+    {
+        _mean = mean;
+        _variance = variance;
+        _min = min;
+        _max = max;
+    }
+
+    /// <summary>
+    /// Generate a normal distributed duration.
+    /// </summary>
+    public int GetDuration(Random? random)
+    {
+        random ??= new Random();
+        int duration = (int)Math.Round(Normal.Sample(random, _mean, _variance));
+        if (_min.HasValue && duration < _min.Value)
+        {
+            duration = _min.Value;
+        }
+        if (_max.HasValue && duration > _max.Value)
+        {
+            duration = _max.Value;
+        }
+        return duration;
+    }
+}
